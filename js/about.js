30a7b69cab2fcaa6938e14c694e6354db03b4fa5
Darius Lung <dariuslung@hotmail.com>
2025-02-20 03:40:07 +0800

    Add picture to experience

---
+++ js/about.js
@@ -0,0 +1,11 @@
+// import lightGallery from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/+esm'
+// import lgZoom from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/plugins/zoom/+esm';
+// import lgThumbnail from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/plugins/thumbnail/+esm';
+
+
+// lightGallery(document.getElementById('abt_gallery'), {
+//     plugins: [lgZoom, lgThumbnail],
+//     licenseKey: '0000-0000-000-0000',
+//     speed: 500,
+//     thumbnail: true,
+// });
