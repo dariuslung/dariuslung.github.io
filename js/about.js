c8b131f522bce0e3c7a64217a1d2216b446aeb7b
Darius Lung <dariuslung@hotmail.com>
2025-02-25 22:46:35 +0800

    Carousel for about gallery

--- js/about.js
+++ js/about.js
@@ -1,11 +1,20 @@
-// import lightGallery from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/+esm'
-// import lgZoom from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/plugins/zoom/+esm';
-// import lgThumbnail from 'https://cdn.jsdelivr.net/npm/lightgallery@2.8.2/plugins/thumbnail/+esm';
+Fancybox.bind('[data-fancybox]', {
+        // Your custom options
+});    
 
+const about_carousel = new Carousel(document.getElementById('card_slider'), {
+    Navigation: {
+        prevTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 5l-7 7 7 7"/><path d="M4 12h16"/></svg>',
+        nextTpl:
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M4 12h16"/><path d="M13 5l7 7-7 7"/></svg>',
+    },
+    infinite: true,
+    center: true,
+    slidesPerPage: 1,
+    transition: false,
+});
 
-// lightGallery(document.getElementById('abt_gallery'), {
-//     plugins: [lgZoom, lgThumbnail],
-//     licenseKey: '0000-0000-000-0000',
-//     speed: 500,
-//     thumbnail: true,
-// });
+setTimeout(() => {
+    about_carousel.reInit();
+}, 50);
